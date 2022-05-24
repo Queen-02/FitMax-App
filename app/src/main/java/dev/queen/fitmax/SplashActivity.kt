aa8f8c3489846995335135acd8a7f4b9332367c1
Queen-02 <queenzyblack176@gmail.com>
2022-05-24 20:04:14 +0300

    made changes to theme file, manifest and splash activity

--- app/src/main/java/dev/queen/fitmax/SplashActivity.kt
+++ app/src/main/java/dev/queen/fitmax/SplashActivity.kt
@@ -1,11 +1,15 @@
 package dev.queen.fitmax
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
