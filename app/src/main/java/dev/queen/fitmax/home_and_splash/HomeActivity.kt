9a2394464085c505931eef66bb97e879d321bcce
Queen-02 <queenzyblack176@gmail.com>
2022-07-12 15:08:46 +0300

    made changes to home activity

--- app/src/main/java/dev/queen/fitmax/home_and_splash/HomeActivity.kt
+++ app/src/main/java/dev/queen/fitmax/home_and_splash/HomeActivity.kt
@@ -2,8 +2,6 @@ package dev.queen.fitmax.home_and_splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.FragmentContainerView
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import dev.queen.fitmax.R
 import dev.queen.fitmax.databinding.ActivityHomeBinding
 import dev.queen.fitmax.fragments.PlanFragment
